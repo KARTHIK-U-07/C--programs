2e9a2328b1a4ab198e656ca6ea19361561979112
KARTHIK-07 <ukarthikyadav0003@gmail.com>
2025-12-22 15:47:10 +0530

    Update Pascal triangle.c

--- Pascal triangle.c
+++ Pascal triangle.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int n = 5;    
+    int n;
+    printf("Enter  No of lines:");
+    scanf("%d",&n);
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n - i; j++) {
             printf("  ");
