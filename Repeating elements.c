b2cd5f0b95461433332a83ae4a8db3c75893ccdd
KARTHIK-07 <ukarthikyadav0003@gmail.com>
2025-12-22 16:05:31 +0530

    Create Repeating elements.c

---
+++ Repeating elements.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Repeating elements are: ");
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                printf("%d ", arr[i]);
+                break; // Avoid printing the same element multiple times
+            }
+        }
+    }
+    return 0;
+}
