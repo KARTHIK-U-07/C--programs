f8fd6ae45be66d85b3f57058d2ad4c4068953a44
KARTHIK-07 <ukarthikyadav0003@gmail.com>
2025-12-24 15:06:32 +0530

    Add array element deletion functionality
    Implement a program to delete an element from an array based on user input.

---
+++ Delete element in array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n, i, pos;
+    int arr[100];
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter position to delete (1 to %d): ", n);
+    scanf("%d", &pos);
+    if (pos < 1 || pos > n) {
+        printf("Invalid position!\n");
+    } else {
+        // Shift elements to the left
+        for (i = pos - 1; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        n
